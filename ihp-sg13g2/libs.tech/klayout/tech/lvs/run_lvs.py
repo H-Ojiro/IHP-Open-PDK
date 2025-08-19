f626bcbcb34462235db08fd63cde31e465eeb8dc
H-Ojiro <h.ojiro@shuharisystem.com>
2025-08-19 10:55:02 +0900

    Add svs flow
    Signed-off-by: H-Ojiro <h.ojiro@shuharisystem.com>

--- ihp-sg13g2/libs.tech/klayout/tech/lvs/run_lvs.py
+++ ihp-sg13g2/libs.tech/klayout/tech/lvs/run_lvs.py
@@ -24,6 +24,7 @@ Usage:
     [--no_net_names] [--spice_comments] [--net_only] [--no_simplify]
     [--no_series_res] [--no_parallel_res] [--combine_devices] [--top_lvl_pins]
     [--purge] [--purge_nets] [--verbose]
+    [--skip_extract]
 
 Options:
     --help -h                           Displays this help message.
@@ -43,6 +44,7 @@ Options:
     --purge                             Removes unused nets from both layout and schematic netlists.
     --purge_nets                        Purges floating nets from both layout and schematic netlists.
     --verbose                           Enables detailed rule execution logs for debugging purposes.
+    --skip_extract                      Skip extract and using existing extracted.cir net
 """
 
 from docopt import docopt
@@ -210,7 +212,8 @@ def generate_klayout_switches(arguments, layout_path, netlist_path):
         "verbose": "true" if arguments.get("--verbose") else "false",
         "topcell": get_run_top_cell_name(arguments, layout_path),
         "input": os.path.abspath(layout_path),
-        "schematic": os.path.abspath(netlist_path)
+        "schematic": os.path.abspath(netlist_path),
+        "skip_extract": "true" if arguments.get("--skip_extract") else "false",
     }
 
     return switches
